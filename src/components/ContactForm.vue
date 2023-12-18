861ebcbd308cc77c44e900576c42cfe17dfe669d
lpodev <lpodevco@gmail.com>
2023-12-18 08:11:58 +0100

    Fix typo in contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -14,7 +14,7 @@
           <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sujet</label>
           <input type="text" id="subject"
                  class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                 placeholder="Quel est le sujet de votre contact" required>
+                 placeholder="Quel est le sujet de votre contact ?" required>
         </div>
         <div class="sm:col-span-2">
           <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Votre message</label>
